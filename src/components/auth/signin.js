7fc60609a595d76520c7fa9eb48e4916406878af
Christine Maynard <christinekrm02@gmail.com>
2021-02-04 16:25:57 -0500

    Setup Redux Form Also add devtools to createStoreWithMiddleWare

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
 class SignIn extends Component {
   render() {
@@ -6,4 +7,7 @@ class SignIn extends Component {
   }
 }
 
+SignIn = reduxForm({
+  form: "SignIn",
+})(SignIn);
 export default SignIn;
