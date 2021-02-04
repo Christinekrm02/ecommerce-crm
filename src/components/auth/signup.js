5d4f25ab71d7edcc2e54faa9b3ecfab4fccc9cd3
Christine Maynard <christinekrm02@gmail.com>
2021-02-04 16:14:06 -0500

    Build signin and signup routes and components

---
+++ src/components/auth/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return <div className="sign-up">Click here to sign up</div>;
+  }
+}
+
+export default SignUp;
