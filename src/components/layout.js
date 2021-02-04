0d5ab37990bd755e33480029d1bfa8341c590733
Christine Maynard <christinekrm02@gmail.com>
2021-02-04 15:38:30 -0500

    Build hlayout and header nav components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 
+import Header from "./header-nav/header";
+import Navbar from "./header-nav/navbar";
+
 class Layout extends Component {
   render() {
-    return <div className="app">{this.props.children}</div>;
+    return (
+      <div className="layout">
+        {this.props.children}
+        <Header />
+        <Navbar />
+      </div>
+    );
   }
 }
 
