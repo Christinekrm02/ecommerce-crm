52b81cc784bbe413150ea5412c314c0d5afc8901
Christine Maynard <christinekrm02@gmail.com>
2021-02-05 10:48:57 -0500

    Build HeaderNavbar reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as form } from "redux-form";
+import HeaderNavbar from "./header-navbar-reducer";
 
 const rootReducer = combineReducers({
   form,
+  HeaderNavbar,
 });
 
 export default rootReducer;
