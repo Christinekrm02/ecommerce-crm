734f0386456af1e7830d27f4cb579644ee80e469
Christine Maynard <christinekrm02@gmail.com>
2021-02-04 22:14:33 -0500

    Build QuickLinksDetails Component QuickLinksDetails Component lists links for user account information and pushes each link thru history to appropriate page

--- src/components/auth/signin-form.js
+++ src/components/auth/signin-form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { FormInput, FormButton } from "../form-fields";
+import QuickLinksDetails from "../quicklinks-details";
 
 class SignInForm extends Component {
   render() {
@@ -8,6 +9,23 @@ class SignInForm extends Component {
       /* obj passed in from signin.js. i.e sign-in__form */
     }
     const { className, handleSubmit } = this.props;
+    const links = [
+      {
+        _id: 0,
+        title: "Not registered yet? Create an account",
+        onClick: () => console.log("click here"),
+      },
+      {
+        _id: 1,
+        title: "Forgot account email?",
+        onClick: () => console.log("forgot user info"),
+      },
+      {
+        _id: 2,
+        title: "Forgot password?",
+        onClick: () => console.log("forgot password"),
+      },
+    ];
     return (
       <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
         <Field
@@ -35,6 +53,11 @@ class SignInForm extends Component {
           title="Login"
           placeholder="login,"
           component={FormButton}
+        />
+        <QuickLinksDetails
+          className="sign-in-form__details"
+          title="QuickLinks"
+          links={links}
         />
       </form>
     );
