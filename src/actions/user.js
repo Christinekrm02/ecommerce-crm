94418f96b123107b96977b5c9a70f27b53ba1dfd
Christine Maynard <christinekrm02@gmail.com>
2021-02-07 12:24:30 -0500

    Add styles to PurchaseHistory component TODO: click event on purchase item on summary to render purchase detail

--- src/actions/user.js
+++ src/actions/user.js
@@ -5,13 +5,69 @@ export function fetchUserPurchases() {
     payload: [
       {
         _id: 0,
-        title: "Purchase one",
-        amount: 9,
+        total: 9,
+        orderNumber: "A00SSB233",
+        orderDate: new Date(),
+        creditCard: "-000",
+        user: {
+          shippingAddress: "555 Twopiece Street",
+          name: "Christine M",
+        },
       },
       {
         _id: 1,
-        title: "Purchase two",
-        amount: 19.52,
+        total: 23.77,
+        orderNumber: "890SSB233",
+        orderDate: new Date(),
+        creditCard: "-033",
+        user: {
+          shippingAddress: "90 Wessone Street",
+          name: "Sade T",
+        },
+      },
+      {
+        _id: 3,
+        total: 22,
+        orderNumber: "A00SSCC565",
+        orderDate: new Date(),
+        creditCard: "-078",
+        user: {
+          shippingAddress: "879A Boyland Street",
+          name: "Jasmine T",
+        },
+      },
+      {
+        _id: 4,
+        total: 29.44,
+        orderNumber: "A00BGH233",
+        orderDate: new Date(),
+        creditCard: "-030",
+        user: {
+          shippingAddress: "908 Notchiham ",
+          name: "Adrew W",
+        },
+      },
+      {
+        _id: 5,
+        total: 99.44,
+        orderNumber: "A908BGH233",
+        orderDate: new Date(),
+        creditCard: "-234",
+        user: {
+          shippingAddress: "57 Layne Ave",
+          name: "Lizze R",
+        },
+      },
+      {
+        _id: 6,
+        total: 98.44,
+        orderNumber: "A908BGABH7",
+        orderDate: new Date(),
+        creditCard: "-786",
+        user: {
+          shippingAddress: "92 Newlaine",
+          name: "Sadey U",
+        },
       },
     ],
   };
