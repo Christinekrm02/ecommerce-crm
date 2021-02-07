b616456ca625ccddb0d08a4d839df5ef144d9d12
Christine Maynard <christinekrm02@gmail.com>
2021-02-07 11:12:15 -0500

    Create PurchaseDetail children The children are history and detail TODO: create actions for details

--- src/components/account/purchase-history.js
+++ src/components/account/purchase-history.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PageTitle from "../page-title";
+import PurchaseDetail from "./purchase-detail";
+import Purchases from "./purchases";
 
 class PurchaseHistory extends Component {
   render() {
@@ -9,6 +11,9 @@ class PurchaseHistory extends Component {
           className="purchase-history__page-title"
           title="Purchase History"
         />
+        <Purchases className="purchase-history__purchases" />
+        <PurchaseDetail className="purchase-history__detail " />
+        <div className="purchase_history__bottom-border"></div>
       </div>
     );
   }
