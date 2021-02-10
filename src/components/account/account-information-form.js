1ea74dfc1c92ba1a557f2490294f58190a5b6280
Christine Maynard <christinekrm02@gmail.com>
2021-02-10 00:15:19 -0500

    Build change password button for AccountInformationForm

--- src/components/account/account-information-form.js
+++ src/components/account/account-information-form.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
-import { FormInput, FormButton } from "../form-fields";
+import { FormInput, FormButton, LongGrayButton } from "../form-fields";
 
 import history from "../../history";
 
@@ -12,9 +12,19 @@ class AccountInformationForm extends Component {
     const { className, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
+      <form
+        onSubmit={handleSubmit}
+        className={`${className} account-information-form`}>
         <Field
-          className="sign-in-form__email"
+          className="account-information-form__name"
+          name="name"
+          type="name"
+          title="Name"
+          placeholder="Name"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form___email"
           name="email"
           type="email"
           title="Email"
@@ -22,22 +32,46 @@ class AccountInformationForm extends Component {
           component={FormInput}
         />
         <Field
-          className="sign-in-form__password"
-          name="password"
-          type="password"
-          title="Password"
-          placeholder="Password"
+          className="account-information-form__street-address"
+          name="address"
+          type="address"
+          title="Street Address"
+          placeholder="Street Address"
           component={FormInput}
         />
-        <div className="sign-in-form__line"></div>
         <Field
-          className="sign-in-form__login"
-          onClick={() => history.push("/account")}
-          name="login"
-          type="submit"
-          title="Login"
-          placeholder="login,"
-          component={FormButton}
+          className="account-information-form__city"
+          name="city"
+          type="city"
+          title="City"
+          placeholder="City"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form__state"
+          name="state"
+          type="state"
+          title="State"
+          placeholder="State"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form__zipcode"
+          name="zipcode"
+          type="zipcode"
+          title="Zipcode"
+          placeholder="Zip"
+          component={FormInput}
+        />
+
+        <Field
+          className="account-information-form__change-password"
+          onClick={() => console.log("show passwords")}
+          name="change-password"
+          type="button"
+          title="Change Password"
+          labelTitle="Password"
+          component={LongGrayButton}
         />
       </form>
     );
