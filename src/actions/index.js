cdb252a08b095e4c750e6a8d60b2d1ea6b6182bb
Christine Maynard <christinekrm02@gmail.com>
2021-02-16 09:30:19 -0500

    Build Shop reducer and fetch Shop categories Can reuse SET_NAVBAR_LINKS component

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,12 @@ import {
 } from "./headerNavbar";
 
 import { fetchUserPurchases, setPurchaseDetail } from "./user";
+import { fetchShopCategories } from "./shop";
 export {
   setHeaderLinks,
   setNavbarLinks,
   changeNavbarActive,
   fetchUserPurchases,
   setPurchaseDetail,
+  fetchShopCategories,
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import { reducer as form } from "redux-form";
 import headerNavbar from "./header-navbar-reducer";
+import shop from "./shop-reducer";
 import user from "./user-reducer";
 
 const rootReducer = combineReducers({
   form,
   headerNavbar,
   user,
+  shop,
 });
 
 export default rootReducer;
