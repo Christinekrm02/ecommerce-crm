44e60cfb3091df6426065792bd4e87dccc21f9d0
Christine Maynard <christinekrm02@gmail.com>
2021-02-16 09:02:52 -0500

    Add Shop component and build Shop route

---
+++ src/components/shop/shop.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+class Shop extends Component {
+  componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: "Login",
+        path: "/signup",
+      },
+    ];
+    this.props.setHeaderLinks(headerLinks);
+    //fetch shop products action creator
+    //set navbar links
+    //filter product with links
+    //set header navbar links
+  }
+
+  render() {
+    return <div className="shop">Shop</div>;
+    //searchbar
+    //product component
+    //cart button
+  }
+}
+function mapStateToProps(state) {
+  return { state };
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+export default Shop;
